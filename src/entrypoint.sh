45904f7b3127fbeec36679799509dfc3dfe562f3
Brian Douglas <bdougie@users.noreply.github.com>
2020-07-26 11:56:01 -0700

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -10,7 +10,7 @@ readonly NC=$'\e[0m'
 echo "## Initializing git repo..."
 git init
 echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/open-sauced/open-sauced.git
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/open-sauced/open-sauced.git
 echo "### Getting branch"
 BRANCH=${checkout_branch}
 echo "### git fetch $BRANCH ..."
